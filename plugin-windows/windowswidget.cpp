c417d244caefbf25ce02b122d88aae7c18107479
probonopd <probonopd@users.noreply.github.com>
2022-10-30 14:07:09 +0100

    Windows menu now has menu items
    https://github.com/helloSystem/Menu/issues/21

--- plugin-windows/windowswidget.cpp
+++ plugin-windows/windowswidget.cpp
@@ -29,16 +29,51 @@ WindowsWidget::WindowsWidget(QWidget *parent)
     m_menubar->addMenu(m_menu);
     setLayout(layout);
 
-    // Add menu items
-    // m_menu->addAction(dateAction);
-    // connect(dateAction, &QAction::triggered, this, &WindowsWidget::change);
-
     updateWindows();
 
     connect(KWindowSystem::self(), &KWindowSystem::activeWindowChanged, this, &WindowsWidget::updateWindows);
 }
 
 void WindowsWidget::updateWindows()
+
+// FIXME: Instead of doing this whole thing each time the frontmost window changes
+// we could probably keep some internal state and change just what needs to be changed;
+// but writing the code for this is more complex and error-prone; so let's see whether
+// we can get away with this
+
 {
     m_menu->setTitle(applicationNiceNameForWId(KWindowSystem::activeWindow()));
+
+    m_menu->clear();
+
+    QList<unsigned int> distinctPids = {};
+    QList<WId> distinctApps = {};
+
+    // Find out one process ID per application that has at least one window
+    // TODO: No Dock windows
+    for (WId id : KWindowSystem::windows()){
+        KWindowInfo info(id, NET::WMPid, NET::WM2TransientFor | NET::WM2WindowClass | NET::WM2WindowRole);
+        if (! distinctPids.contains(info.pid())) {
+            distinctPids.append(info.pid());
+            distinctApps.append(id);
+        }
+    }
+
+    // Add one menu item for each appliction
+
+    for (WId id : distinctApps){
+        // TODO: If there are multiple windows for the same PID, then add multiple submenus
+        // So don't add an action here, but a submenu which contains all windows that beloong to that PID
+        QString niceName = applicationNiceNameForWId(id);
+        if(niceName == "cyber-dock") // FIXME: Don't harcode, see "No Dock windows" comment above
+            continue;
+        QAction *appAction = m_menu->addAction(niceName);
+        appAction->setCheckable(true);
+        if(id == KWindowSystem::activeWindow())
+                appAction->setChecked(true);
+        // appAction->setIcon(QIcon(KWindowSystem::icon(id))); // Why does this not work? TODO: Get icon from bundle?
+        connect(appAction, &QAction::triggered, this, [appAction, id, this]() {
+            KWindowSystem::activateWindow(id);
+        });
+    }
 }
