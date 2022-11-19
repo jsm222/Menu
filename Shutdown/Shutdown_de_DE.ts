77b3d9645e10d75801a69385971cf43eb0a3575a
probonopd <probonopd@users.noreply.github.com>
2022-11-19 11:08:47 +0100

    Fix libstatusnotifier loading
    Closes https://github.com/helloSystem/Menu/issues/118
    Thanks @jsm222

--- Shutdown/Shutdown_de_DE.ts
+++ Shutdown/Shutdown_de_DE.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="Shutdown_de_DE">
+<TS version="2.1" language="de_DE">
 <context>
     <name>AppMenuWidget</name>
     <message>
@@ -79,15 +79,10 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="59"/>
-        <location filename="../build/Shutdown/Shutdown_autogen/include/ui_mainwindow.h" line="106"/>
-        <source>Esc</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="110"/>
-        <location filename="../build/Shutdown/Shutdown_autogen/include/ui_mainwindow.h" line="101"/>
-        <source>Log out</source>
+        <location filename="mainwindow.ui" line="49"/>
+        <location filename="../build/Shutdown/Shutdown_autogen/include/ui_mainwindow.h" line="103"/>
+        <location filename="../src/mainwindow.cpp" line="49"/>
+        <source>Log Out</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -97,23 +92,27 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="49"/>
-        <location filename="../build/Shutdown/Shutdown_autogen/include/ui_mainwindow.h" line="103"/>
-        <location filename="../src/mainwindow.cpp" line="49"/>
-        <source>Log Out</source>
+        <location filename="mainwindow.ui" line="59"/>
+        <location filename="../build/Shutdown/Shutdown_autogen/include/ui_mainwindow.h" line="106"/>
+        <source>Esc</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="72"/>
         <location filename="../build/Shutdown/Shutdown_autogen/include/ui_mainwindow.h" line="108"/>
         <source>Shut Down</source>
-        <oldsource>Shut down</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="91"/>
         <location filename="../build/Shutdown/Shutdown_autogen/include/ui_mainwindow.h" line="109"/>
         <source>Are you sure you want to shut down your computer now?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="110"/>
+        <location filename="../build/Shutdown/Shutdown_autogen/include/ui_mainwindow.h" line="101"/>
+        <source>Log out</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
